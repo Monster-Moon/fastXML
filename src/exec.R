4419d108bceb0c65ce0e766215dd75333df7f0be
Monster-Moon <msj5589@gmail.com>
2019-02-26 17:34:31 +0900

    First commit
    first update
    code related to prediction is not completed.

---
+++ src/exec.R
@@ -0,0 +1,19 @@
+
+
+setwd('D:\\Project\\git\\fastXML\\src')
+source('xml_function.R')
+
+#### data generate ####
+## train_data
+set.seed(1)
+n = 100L
+input_dim = 5L
+output_dim = 3L
+x_data = matrix(rnorm(n * input_dim), ncol = input_dim)
+beta_star = matrix(rnorm(input_dim * output_dim), nrow = input_dim, ncol = output_dim)
+y_prop =  exp(x_data %*% beta_star)/(1 + exp(x_data %*% beta_star))
+y_data = matrix(rbinom(n * output_dim, size = 1, prob = as.numeric(y_prop)), ncol = output_dim)
+
+## result
+tree_list = fastXML_func(x = x_data, y = y_data, K = 2)
+
---
+++ src/xml_function.R
@@ -0,0 +1,102 @@
+####################################
+############# fastXML ##############
+####################################
+
+rm(list = ls())
+gc(reset = T)
+
+if(!require(dplyr)) install.packages("dplyr")
+require(dplyr)
+
+#### code
+fastXML_func = function(x, y, K, MaxLeaf = 10L)
+{
+  tree_list = vector('list', K)
+  n_id = 1:nrow(x_data)
+  for(k in 1:K)
+  {
+    tree_list[[k]] = grow_node_recursive_func(x = x, y = y, data_inx = n_id, MaxLeaf = MaxLeaf)
+  }
+  return(tree_list)
+}
+
+grow_node_recursive_func = function(x, y, data_inx, MaxLeaf = 10L) ## Return tree
+{
+  if(length(data_inx) <= MaxLeaf)
+  {
+    P = process_leaf_func(y, data_inx)
+    w = NULL
+  }else
+  {
+    split_node_result = split_node_func(x, y, data_inx)
+    w = split_node_result$w
+    n_left_tree = grow_node_recursive_func(x, y, split_node_result$n_left_child, MaxLeaf = MaxLeaf)
+    n_right_tree = grow_node_recursive_func(x, y, split_node_result$n_right_child, MaxLeaf = MaxLeaf)
+  }
+  
+  if(is.null(w))
+  {
+    return(list(P = P))
+  }else{
+    return(list(w = w,
+                n_left_tree = n_left_tree, 
+                n_right_tree = n_right_tree))
+  }
+}
+
+IL_y_func = function(s)
+{
+  return(1/sum(1/log(1 + 1:s)))
+}
+
+split_node_func = function(x, y, data_inx, max_iter = 100L)
+{
+  set.seed(1)
+  delta_next_inx = delta_inx = sample(c(-1, 1), size = length(data_inx), replace = T)
+  y_id = y[data_inx, , drop = F]
+  x_id = x[data_inx, , drop = F]
+  d = ncol(x)
+  L = ncol(y)
+  
+  IL_y = lapply(rowSums(y_id), IL_y_func) %>% unlist()
+  IL_y_y = IL_y * y_id
+  
+  w_init = rep(0, d)
+  # w_init = rnorm(d)
+  break_stack = 0
+  for(i in 1:max_iter)
+  {
+    r_plus_order = order(colSums(0.5 * (1 + delta_inx) * IL_y_y), decreasing = T)
+    r_minus_order = order(colSums(0.5 * (1 - delta_inx) * IL_y_y), decreasing = T)
+    
+    v_plus = log(1 + exp(-x_id %*% w_init)) - IL_y * rowSums(y_id[, r_plus_order] / log(1 + 1:L))
+    v_minus = log(1 + exp(x_id %*% w_init)) - IL_y * rowSums(y_id[, r_minus_order] / log(1 + 1:L))
+    v_sign = sign(v_minus - v_plus)
+    delta_next_inx[v_sign != 0] = v_sign[v_sign != 0]
+    if(all(delta_next_inx == delta_inx))
+    {
+      w_next = optim(w_init, objective_w_func, x = x_id, delta_inx = delta_inx)$par # w update
+      break_stack = break_stack + 1
+      if(break_stack >= 3) break
+    }else{
+      w_next = w_init
+    }
+    delta_inx = delta_next_inx
+  }
+  decision_boundary = x_id %*% w_next
+  n_right_child = data_inx[which(decision_boundary > 0)]
+  n_left_child = data_inx[which(decision_boundary <= 0)]
+  
+  return(list(w = w_next, n_left_child = n_left_child, n_right_child = n_right_child))
+}
+
+objective_w_func = function(w, x, delta_inx)
+{
+  return(sum(abs(w))  + colSums(log(1 + exp(-delta_inx * x %*% w))))
+}
+
+process_leaf_func = function(y, data_inx)
+{
+  return(colMeans(y[data_inx, , drop = F]))
+}
+
